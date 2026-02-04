776bf05b25091412cb6877cf053de2406202b62b
Franzen61 <franat19@gmail.com>
2026-02-04 08:30:08 +0100

    Update app.py

--- app.py
+++ app.py
@@ -376,7 +376,8 @@ with tab4:
     <b>Breadth settoriale</b><br>
     Cyclicals in leadership: <b>{cyc_breadth}/{len(CYCLICALS)}</b> ({cyc_pct:.0f}%)<br>
     Defensives in leadership: <b>{def_breadth}/{len(DEFENSIVES)}</b> ({def_pct:.0f}%)
-
+    <b>Defensive: XLP-XLV-XLU-XLRE  Cyclicals: XLK-XLY-XLF-XLC-XLI-XLE-XLB<B>
+    
     <br><br>
 
     <b>Lettura corrente</b><br>
